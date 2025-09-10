827a9468cffc3abc2fd21f625b5a6c53148f7ad1
mabe4242 <kouiy42@gmail.com>
2025-09-10 17:16:37 +0900

    Fortifyでマルチログイン機能の実装

---
+++ src/resources/views/admin/attendance_index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+{{-- @section('css')
+    <link rel="stylesheet" href="{{ asset('css/admin/attendance_index.css') }}">
+@endsection --}}
+
+@section('content')
+    <x-admin_header></x-admin_header>
+    管理者の一覧画面です。
+@endsection
